fbfa77fbfaf3cc36078fc46e5dc01f1589101260
Kainat Fatima <k.fatima@mudaraba.sa>
2023-11-14 19:32:19 +0500

    Initial commit

---
+++ src/model/FullList.ts
@@ -0,0 +1,43 @@
+import ListItems from "./ListItem";
+interface List {
+    list: ListItems[],
+    load(): void,
+    save(): void,
+    addItem(itemObj: ListItems): void,
+    removeItem(id: string): void,
+    clearList(): void
+}
+
+export default class FullList implements List {
+    static instance: FullList = new FullList();
+    private constructor(private _list: ListItems[] = []) { }
+    get list(): ListItems[] {
+        return this._list;
+    }
+    load(): void {
+        const storedList: string | null = localStorage.getItem("MyList")
+        if (typeof storedList !== "string") return
+        const parsedList: { _id: string, _item: string, _checked: boolean }[] = JSON.parse(storedList)
+        parsedList.forEach(itemObj => {
+            const newListItem = new ListItems(itemObj._id, itemObj._item, itemObj._checked)
+            FullList.instance.addItem(newListItem)
+        })
+    }
+    save(): void {
+        localStorage.setItem("MyList", JSON.stringify(this._list));
+
+    }
+    clearList(): void {
+        this._list = [];
+        this.save();
+    }
+    addItem(itemObj: ListItems): void {
+        this._list.push(itemObj)
+        this.save();
+    }
+    removeItem(id: string): void {
+        this._list = this._list.filter(item => item.id !== id)
+        this.save();
+    }
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'none',
+
+  entry: './src/index.ts',
+
+  output: {
+
+    filename: 'bundle.js',
+
+    path: path.resolve(__dirname, 'dist'),
+
+  },
+
+  module: {
+
+    rules: [
+
+      {
+
+        test: /\.tsx?$/,
+
+        use: 'ts-loader',
+
+        exclude: /node_modules/,
+
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+
+    ],
+
+  },
+
+  resolve: {
+
+    extensions: ['.tsx', '.ts', '.js'],
+
+  },
+
+  devtool: 'source-map',
+
+  devServer: {
+
+    static: './dist',
+
+    compress: true,
+
+    port: 9000,
+
+  },
+
+};
