fafb70f653cb0f165c5a6e8eeb7214e6dfa1abec
Kainat Fatima <k.fatima@mudaraba.sa>
2024-11-04 14:42:34 +0500

    Updated FullList class to move checked items to the end of the list

--- src/model/FullList.ts
+++ src/model/FullList.ts
@@ -11,33 +11,43 @@ interface List {
 export default class FullList implements List {
     static instance: FullList = new FullList();
     private constructor(private _list: ListItems[] = []) { }
+
     get list(): ListItems[] {
         return this._list;
     }
+
     load(): void {
-        const storedList: string | null = localStorage.getItem("MyList")
-        if (typeof storedList !== "string") return
-        const parsedList: { _id: string, _item: string, _checked: boolean }[] = JSON.parse(storedList)
+        const storedList: string | null = localStorage.getItem("MyList");
+        if (typeof storedList !== "string") return;
+        const parsedList: { _id: string, _item: string, _checked: boolean }[] = JSON.parse(storedList);
         parsedList.forEach(itemObj => {
-            const newListItem = new ListItems(itemObj._id, itemObj._item, itemObj._checked)
-            FullList.instance.addItem(newListItem)
-        })
+            const newListItem = new ListItems(itemObj._id, itemObj._item, itemObj._checked);
+            FullList.instance.addItem(newListItem);
+        });
+        this.sortList();
     }
+
     save(): void {
         localStorage.setItem("MyList", JSON.stringify(this._list));
-
     }
+
     clearList(): void {
         this._list = [];
         this.save();
     }
+
     addItem(itemObj: ListItems): void {
-        this._list.push(itemObj)
-        this.save();
-    }
-    removeItem(id: string): void {
-        this._list = this._list.filter(item => item.id !== id)
+        this._list.push(itemObj);
+        this.sortList();
         this.save();
     }
 
+    removeItem(id: string): void {
+        this._list = this._list.filter(item => item.id !== id);
+        this.save();
+    }
+
+    private sortList(): void {
+        this._list.sort((a, b) => Number(a.checked) - Number(b.checked));
+    }
 }
